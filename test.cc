f771fee144e145c12fcf047f2ce9e5559dcc63e8
betyoucantguessmyusername <36983270+betyoucantguessmyusername@users.noreply.github.com>
2018-10-10 19:56:00 -0700

    printed what tests are doing
    prints what it expects then gets instead of a bunch of ambiguous data

--- test.cc
+++ test.cc
@@ -28,41 +28,48 @@ int main() {
 	Cache* c = new Cache(2);
 
 	// test set
+	cout << "\ninserting 'key':val int x (21)\n";
 	int x = 21;
 	set_test(c, "key", &x, space_used_test(c));
 
 	// test space_used
-	cout << space_used_test(c) << '\n';
+	cout << "space used after 1 insert: " << space_used_test(c) << '\n';
 
 	// test get present element
 	Cache::index_type size = space_used_test(c);
 	Cache::val_type val_addr = get_test(c, "key", size);
-	cout << val_addr << '\n';
+	cout << "address of val at key: " << val_addr << '\n';
 
 	// test set-overwrite
+	cout << "\ninserting 'key':val int x (16) [should overwrite]\n";
 	x = 16;
 	set_test(c, "key", &x, space_used_test(c));
 	// see if it overwrote
 	size = space_used_test(c);
+	cout << "looking up 'key': ";
 	val_addr = get_test(c, "key", size);
 	cout << val_addr << '\n';
 	//size should still be 1
-	cout << space_used_test(c) << '\n';
+	cout << "size should still be 1: " << space_used_test(c) << '\n';
 
 	//test get an absent element
+	cout << "\nlooking up absent element 'keyAbsent': ";
 	val_addr = get_test(c, "keyAbsent", size);
 	cout << val_addr << '\n';
 
 	//test del
+	cout << "\ndeleting 'key'\n";
 	del_test(c, "key");
 	//see if del worked
+	cout << "looking up 'key': ";
 	size = space_used_test(c);
 	val_addr = get_test(c, "key", size);
 	cout << val_addr << '\n';
 	//ensure memused is updated
-	cout << space_used_test(c) << '\n';
+	cout << "space used should be 0: " << space_used_test(c) << '\n';
 
 	//test evict (add 3 el.s, check that 1 gets evicted)
+	cout << "\ntesting evict, adding 3 el.s to a map w max_load 2...\n";
 	x=17;
 	set_test(c, "key2", &x, space_used_test(c));
 	int y = 18;
@@ -70,6 +77,7 @@ int main() {
 	int z = 19;
 	set_test(c, "key4", &z, space_used_test(c));
 	// size should be 2 not 3
+	cout << "map size is: ";
 	cout << space_used_test(c) << '\n';
 
 
