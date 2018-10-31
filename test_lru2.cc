20fd0a9f27dc9eac33d934ea5090a2358c7596cc
Ezra Schwartz <ezraschwartz@reed.edu>
2018-10-30 18:21:46 -0700

    free cache

--- test_lru2.cc
+++ test_lru2.cc
@@ -1,4 +1,4 @@
-// Joe Meyer
+// Joe Meyer & Ezra Schwartz
 
 #include "cache_lru.cc"
 
@@ -44,14 +44,9 @@ void test_hasher() {
 	betterHasher myHasher = betterHasher(bound);
 
 	uint32_t keyhash = myHasher("key");
-	uint32_t ckey3hash = myHasher("ckey3");
 	uint32_t newkeyhash = myHasher("key");
-	uint32_t newckey3hash = myHasher("ckey3");
 	assert(newkeyhash==keyhash);
-	assert(newckey3hash==ckey3hash);
-
 }
-
 
 // insert item into cache, query it, assert both value is unchanged
 void test_set_insert() {
@@ -68,6 +63,7 @@ void test_set_insert() {
 
 	// query item and assert value is correct
 	assert(get_interface(myCache, "key", size) == 1 && "'key' value is wrong");
+	free(myCache);
 }
 
 // fill cache, do insert and query it
@@ -84,6 +80,7 @@ void test_set_insert_full() {
 	val = 3;
 	myCache->set("key3", &val, size);
 	assert(get_interface(myCache, "key3", size) == 3 && "set fails on full cache");
+	free(myCache);
 }
 
 // set element, overwrite it, query it
@@ -98,6 +95,7 @@ void test_set_overwrite() {
 	val = 2;
 	myCache->set("key", &val, size);
 	assert(get_interface(myCache, "key", size) == 2 && "overwrite fails");
+	free(myCache);
 }
 
 // set element, overwrite it with different size, query it
@@ -114,6 +112,7 @@ void test_set_overwrite_dif_size() {
 	myCache->set("key", &val, size);
 	assert(get_interface(myCache, "key", size) == 2 && "overwriting w diff size fails");
 
+	free(myCache);
 }
 
 // test that evictor uses first in first out policy
@@ -123,15 +122,33 @@ void test_evictor_fifo() {
 	uint32_t bound = 2;
 	betterHasher myHasher = betterHasher(bound);
 	Cache* myCache = new Cache(cache_length*size, [](){ return 0; }, myHasher);
+	uint32_t val = 1;
+	myCache->set("key1", &val, size);
+	val = 2;
+	myCache->set("key2", &val, size);
+	val = 3;
+	myCache->set("key3", &val, size);
+	assert(get_interface(myCache, "key1", size) == -1 && "FIFO-eviction failed");
+	free(myCache);
 }
 
-// tests the evictor takes out last item
+// tests that overwrite updates eviction queue position 
 void test_evictor_lru() {
 	uint32_t cache_length = 2;
 	uint32_t size = sizeof(uint32_t);
 	uint32_t bound = 2;
 	betterHasher myHasher = betterHasher(bound);
 	Cache* myCache = new Cache(cache_length*size, [](){ return 0; }, myHasher);
+	uint32_t val = 1;
+	myCache->set("key1", &val, size);
+	val = 2;
+	myCache->set("key2", &val, size);
+	val = 3;
+	myCache->set("key1", &val, size);
+	val = 4;
+	myCache->set("key3", &val, size);
+	assert(get_interface(myCache, "key2", size) == -1 && "LRU-eviction failed");
+	free(myCache);
 }
 
 // checks if get return the correct item
@@ -148,6 +165,7 @@ void test_get_present() {
 	myCache->set(key, &val, val_size);
 	int32_t get_data = get_interface(myCache, key, val_size);
 	assert(get_data == 42 && "did not get correct variable.");
+	free(myCache);
 }
 
 //checks that using get on an absent item returns a nullptr
@@ -162,6 +180,7 @@ void test_get_absent() {
 	Cache::index_type val_size = sizeof(uint32_t);
 	int32_t get_data = get_interface(myCache, key, val_size);
 	assert(get_data == -1 && "Getting an absent variable should return nullptr");
+	free(myCache);
 }
 
 //checks that using get on a deleted item returns a nullptr
@@ -179,6 +198,7 @@ void test_get_deleted() {
 	myCache->del(key);
 	int32_t get_data = get_interface(myCache, key, val_size);
 	assert(get_data == -1 && "Getting a deleted variable should return nullptr");
+	free(myCache);
 }
 
 //checks that delete works
@@ -195,6 +215,7 @@ void test_delete_present() {
 	myCache->set(key, &val, val_size);
 	myCache->del(key);
 	assert(myCache->space_used() == 0 && "Space should be empty since key was deleted.");
+	free(myCache);
 }
 
 //checks that we don't crash when we delete something absent
@@ -211,6 +232,7 @@ void test_delete_absent() {
 	Cache::index_type space_used = myCache->space_used();
 	myCache->del(key);
 	assert(space_used == myCache->space_used() && "Deleting an absent key doesn't impact the Cache.");
+	free(myCache);
 }
 
 //checks that initial space used is 0.
@@ -223,6 +245,7 @@ void test_space_used() {
 
 	Cache::index_type space_used = myCache->space_used();
 	assert(space_used == 0 && "Cache should initially be empty");
+	free(myCache);
 }
 
 // insert element, check space used
@@ -239,6 +262,7 @@ void test_space_used_insert() {
 	myCache->set(key, &val, val_size);
 	Cache::index_type space_used = myCache->space_used();
 	assert(space_used == val_size && "Space used should be the size.");
+	free(myCache);
 }
 
 // insert element, check space used, delete it, check again
@@ -256,10 +280,15 @@ void test_space_used_delete() {
 	myCache->del(key);
 	Cache::index_type space_used = myCache->space_used();
 	assert(space_used == 0 && "Space used should empty now that we've deleted the only element.");
+	free(myCache);
 }
 
 
 int main(){
+	cout << "Running test_hasher() \t\t\t"; 
+	test_hasher();
+	cout << "PASS" << endl;
+
 	cout << "Running test_set_insert() \t\t"; 
 	test_set_insert();
 	cout << "PASS" << endl;
@@ -274,6 +303,14 @@ int main(){
 
 	cout << "Running test_set_overwrite_dif_size() \t"; 
 	test_set_overwrite_dif_size();
+	cout << "PASS" << endl;
+
+	cout << "Running test_evictor_fifo() \t\t";
+	test_evictor_fifo();
+	cout << "PASS" << endl;
+
+	cout << "Running test_evictor_lru() \t\t";
+	test_evictor_lru();
 	cout << "PASS" << endl;
 
 	cout << "Running test_get_present() \t\t";
@@ -308,117 +345,3 @@ int main(){
 	test_space_used_delete();
 	cout << "PASS" << endl;
 }
-
-
-
-
-
-
-// uint32_t main() {
-	/*
-	// initialize Cache obj 'cache_length'
-	uint32_t cache_length = 2;
-
-	cout << "Initializing myHasher with bound 2 [val.s should hash to 1 or 0]...\n";
-
-	betterHasher myHasher = betterHasher(cache_length);
-
-	//test hasher
-	cout << "'key' hashes to " << myHasher("key") << '\n';
-	cout << "'ckey3' hashes to " << myHasher("ckey3") << "\n\n";
-
-	cout << "Creating cache using myHasher w maxmem 2...\n";
-	cout << "(values by default are 4 bytes long)\n";
-
-	// size is size of vals (which are ints)
-	uint32_t size = sizeof(uint32_t);
-
-
-	Cache* myCache = new Cache(cache_length*size, [](){ return 0; }, myHasher);
-
-	assert(space_used_test(myCache)==0 && "empty cache should have no elements");
-
-
-	uint32_t x = 21;
-	set_test(myCache, "key", &x, size);
-	assert(space_used_test(myCache)==size && "space used after 1 insert should be 4 bytes");
-	// get present element
-	get_interface(myCache, "key", size);
-	assert(space_used_test(myCache)==size && "'get' should not affect space used");
-	cout << '\n';
-
-	// overwrite present element
-	x = 16;
-	set_test(myCache, "key", &x, size);
-	// see if it overwrote
-	get_interface(myCache, "key", size);
-	//size should still be 1
-	assert(space_used_test(myCache)==size && "overwrite should not affect space used");
-	cout << '\n';
-
-	//test get/del an absent element
-	get_interface(myCache, "keyAbsent", size);
-	del_test(myCache, "keyAbsent");
-	assert(space_used_test(myCache)==size && "del.ing absent el. should not affect space used");
-
-	cout << '\n';
-	//test del present element
-	get_interface(myCache, "key", size);
-	del_test(myCache, "key");
-	//see if del worked
-	assert(space_used_test(myCache)==0 && "del.ing present el. should decrement space used");
-	get_interface(myCache, "key", size);
-	
-
-	//test evict (add 3 el.s, check that 1 gets evicted) [all same size => tests FIFO-evict]
-	cout << "\ntesting Fifo-evict, adding 3 same-sized el.s to a map w max_load 2...\n";
-	x=17;
-	set_test(myCache, "key", &x, size);
-	x = 18;
-	set_test(myCache, "key1", &x, size);
-	cout << "should evict 'key' next since all same size vals => FIFO\n";
-	x = 19;
-	set_test(myCache, "key2", &x, size);
-	// size should be 2 not 3
-	cout << "map size should be 2*4 = 8: ";
-	assert(space_used_test(myCache)==2*size && "Should add 3, evict 1");
-	cout << '\n';
-
-	get_interface(myCache, "key", size);
-	get_interface(myCache, "key1", size);
-	get_interface(myCache, "key2", size);
-	cout << '\n';
-	del_test(myCache, "key1");
-	del_test(myCache, "key2");	
-	space_used_test(myCache);
-	cout << '\n';
-
-	// tests set, LRU evict, and overwrite (including overwriting dif. size)
-	cout << "testing LRU-evict, overwrite: inserting 4 el.s in ascending size\n";
-	cout << "(1 should get evicted and 1 overwritten)\n";
-	cout << "(the first el. is 4 bytes and each el. is 1 byte longer than the last)\n";
-	x=31;
-	set_test(myCache, "ckey1", &x, size);
-	x = 32;
-	set_test(myCache, "ckey2", &x, size+1);
-	x = 33;
-
-	cout << "'ckey2' should get evicted next bc it's biggest\n";
-	set_test(myCache, "ckey3", &x, size+2);
-	x = 34;
-	cout << "overwriting 'ckey3' with dif. sized value\n";
-	set_test(myCache, "ckey3", &x, size+3);
-
-	cout << "space used should be 4 + (4+3) = 11: ";
-	assert(space_used_test(myCache)==(size + (size + 3)) && "Should add 3, evict 1, overwrite 1");
-	cout << '\n';
-
-	get_interface(myCache, "ckey1", size);
-	get_interface(myCache, "ckey2", size);
-	get_interface(myCache, "ckey3", size);
-
-	free(myCache);
-	*/
-
-// }
-
